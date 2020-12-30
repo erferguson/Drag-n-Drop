a3d2d5e4c16eafbfc300ffb6edc2cd879b732ec6
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-29 18:41:34 -0800

    create group 1, group 2 and blank with associated CSS

--- drag-drop/src/App.js
+++ drag-drop/src/App.js
@@ -6,11 +6,38 @@ function App() {
       <header className="App-header">
         <div className="drag-n-drop">
           <div className="dnd-group">
+            <div className="group-title">Group 1</div>
             <div className="dnd-item">
               <div>
-                <p>ITEM</p>
+                <p>ITEM 1</p>
               </div>
             </div>
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 2</p>
+              </div>
+            </div>
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 3</p>
+              </div>
+            </div>
+          </div>
+          <div className="dnd-group">
+          <div className="group-title">Group 2</div>
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 1</p>
+              </div>
+            </div>
+            <div className="dnd-item">
+              <div>
+                <p>ITEM 2</p>
+              </div>
+            </div>
+          </div>
+          <div className="dnd-group">
+            
           </div>
         </div>
       </header>
